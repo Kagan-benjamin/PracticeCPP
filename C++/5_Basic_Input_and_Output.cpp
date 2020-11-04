9d2636919b78fab4053ea3a8810586afddbc103e
Kagan-benjamin <kagan.benjamin@gmail.com>
2020-11-04 16:25:48 -0500

    Basic Input and Output c++

---
+++ C++/5_Basic_Input_and_Output.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main() {
+//    cout << "Hello" << " World!\n";
+//    cout << "Hello\nOut\nThere\n";
+
+    int num1;
+    int num2;
+//    double num3;
+    
+    cout << "Enter 2 ints separated with a space: ";
+    cin >> num1 >> num2;
+    cout << "You entered " << num1 << " and " << num2 << endl;
+    
+//    cout << "Enter a 1st integer: ";
+//    cin >> num1;
+//    
+//    cout << "Enter a 2nd integer: ";
+//    cin >> num2;
+//    
+//    cout << "You entered: " << num1 << " and " << num2 << endl;
+
+
+//    cout << "Enter a double ";
+//    cin >> num3;
+
+//    cout << "You entered " << num3 << endl;
+    return 0;
+}
