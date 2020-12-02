60eb937acddf940eeacba28ee9a3d2ab9f887591
Kagan-benjamin <kagan.benjamin@gmail.com>
2020-12-02 13:00:43 -0500

    C++ constructor parameters and default values

--- C++/13_OOP_Classes_and_Objects/ConstructorDelegation.cpp
+++ C++/13_OOP_Classes_and_Objects/ConstructorDelegation.cpp
@@ -10,30 +10,35 @@ private:
     int health;
     int xp;
 public:
-    Player();
-    Player(std::string name_val);
-    Player(std::string name_val, int health_val, int xp_val);
+    Player(std::string name_val = "None", 
+           int health_val = 0,    // default parameters 
+           int xp_val = 0);
 };
 
-Player::Player()             // delegating constructor
-    : Player{"None", 0, 0} {
-        cout << "No-args Constructor" << endl;
-}
+//Player::Player()             // delegating constructor
+//    : Player{"None", 0, 0} {
+//        cout << "No-args Constructor" << endl;
+//}
+//
+//Player::Player(std::string name_val) // delegating constructor
+//    : Player{name_val, 0, 0} {
+//        cout << "1-arg Constructor" << endl;
+//}
+//
+//Player::Player(std::string name_val, int health_val, int xp_val)
+//    : name{name_val}, health{health_val}, xp{xp_val} {
+//        cout << "3-arg Constructor" << endl;
+//}
 
-Player::Player(std::string name_val) // delegating constructor
-    : Player{name_val, 0, 0} {
-        cout << "1-arg Constructor" << endl;
-}
-
-Player::Player(std::string name_val, int health_val, int xp_val)
+Player::Player(string name_val, int health_val, int xp_val)
     : name{name_val}, health{health_val}, xp{xp_val} {
-        cout << "3-arg Constructor" << endl;
 }
 
 
 int main() {
 	Player empty;
     Player ben{"Ben"};
+    Player hero{"Hero",85};
     Player villain{"Villain",100,75};
     
     return 0;
