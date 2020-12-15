77e428c74f9fe781b0e093d118ffd7072c94fb3e
Kagan-benjamin <kagan.benjamin@gmail.com>
2020-12-15 11:38:57 -0500

    C++ Deriving Classes from Existing Classes

---
+++ C++/15_Inheritance/DerivingClasses/Account.h
@@ -0,0 +1,19 @@
+#ifndef _ACCOUNT_H_
+#define _ACCOUNT_H_
+#include <string>
+
+
+class Account {
+public:
+    double balance;
+    std::string name;
+
+    Account();
+    ~Account();
+
+    void get_balance();
+    void deposit(double amount);
+    void withdraw(double amount);
+};
+
+#endif // _ACCOUNT_H_
