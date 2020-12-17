3ae82cc7b631c8f7f7d6ce37366771c0ce3eb0d7
Kagan-benjamin <kagan.benjamin@gmail.com>
2020-12-17 11:51:18 -0500

    C++ Inheritance: Static Binding of Method Calls and Specification of Class Methods

---
+++ C++/15_Inheritance/RedefiningBaseClassMethods/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include "Account.h"
+#include "Savings_Account.h"
+
+using namespace std;
+
+int main() {
+    cout << "--------------- Account Class ---------------" << endl;
+    Account a1{1000.0};
+    cout << a1 << endl;
+    
+    a1.deposit(500.0);
+    cout << a1 << endl;
+    
+    a1.withdraw(1000);
+    cout << a1 << endl;
+    
+    a1.withdraw(5000.0);
+    cout << a1 << endl;
+    
+    cout << "--------------- Savings Account Class ---------------" << endl;
+    Savings_Account s1{1000, 5.0};
+    cout << s1 << endl;
+    
+    s1.deposit(1000);
+    cout << s1 << endl;
+    
+    s1.withdraw(2000);
+    cout << s1 << endl;
+    
+    s1.withdraw(1000.0);
+    cout << s1 << endl;
+    
+    cout << endl;
+    return 0;
+}
