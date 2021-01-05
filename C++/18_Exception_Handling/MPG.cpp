4d773aa0b57fd5215fee75658a98b721f4318301
Kagan-benjamin <kagan.benjamin@gmail.com>
2021-01-05 13:47:54 -0500

    C++ Exception Handling Simple Example

---
+++ C++/18_Exception_Handling/MPG.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int miles{};
+    int gallons{};
+    double miles_per_gallon{};
+    
+    cout << "Enter the miles: ";
+    cin >> miles;
+    cout << "Enter the gallons: ";
+    cin >> gallons;
+    
+//    miles_per_gallon = miles / gallons;
+    if (gallons != 0){
+    miles_per_gallon = static_cast<double>(miles) / gallons;
+    cout << "Result: " << miles_per_gallon << endl;
+    }
+    else {
+        cerr << "Sorry, can't divide by zero" << endl;
+    }
+    cout << "Bye!" << endl;
+    return 0;
+}
