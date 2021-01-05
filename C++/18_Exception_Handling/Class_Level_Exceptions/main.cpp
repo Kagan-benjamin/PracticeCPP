039aeab26171a5ffe1eb4b43534337c3ab2d68fa
Kagan-benjamin <kagan.benjamin@gmail.com>
2021-01-05 17:14:13 -0500

    C++ Class Level Exceptions and std::exception Class Hierarchy

---
+++ C++/18_Exception_Handling/Class_Level_Exceptions/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <memory>
+#include "Account.h"
+#include "Checking_Account.h"
+#include "Savings_Account.h"
+#include "Trust_Account.h"
+#include "Account_Util.h"
+#include "IllegalBalanceException.h"
+
+using namespace std;
+
+int main() {
+    try {
+       std::unique_ptr<Account> bens_account = 
+            std::make_unique<Checking_Account>("Ben", 10.0);
+       std::cout << *bens_account << std::endl;
+       
+       std::unique_ptr<Account> moes_account = 
+            std::make_unique<Checking_Account>("Moe", -10.0);
+       std::cout << *moes_account << std::endl;
+    }
+    catch (const IllegalBalanceException &ex)
+    {
+        std::cerr << "Couldn't create account - negative balance" << std::endl;
+    }
+    std::cout << "Program completed successfully" << std::endl;
+    return 0;
+}
